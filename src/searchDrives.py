211983ae337184efb3d4deb9ceb9c32c2657850b
MateiDevel <serbanmatei572@gmail.com>
2025-09-29 16:30:52 +0300

    Now monitors USBs

--- src/searchDrives.py
+++ src/searchDrives.py
@@ -1,6 +1,9 @@
 import pyudev
+import threading
 
 context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(subsystem='block', device_type='disk')
 
 def searchDrives(usbSelect):
     usbSelect.clear()
@@ -11,3 +14,16 @@ def searchDrives(usbSelect):
             output = f"{model} ({deviceNode})"
             usbSelect.addItem(output, deviceNode)
             print(f"[WRITEDD] {output}")
+
+    def watch():
+        for device in iter(monitor.poll, None):
+            if 'ID_USB_DRIVER' not in device:
+                continue
+            if device.action == 'add':
+                print(f"[WRITEDD] USB inserted: {device.device_node}")
+                searchDrives(usbSelect)
+            elif device.action == 'remove':
+                print(f"[WRITEDD] USB removed: {device.device_node}")
+                searchDrives(usbSelect)
+
+    threading.Thread(target=watch, daemon=True).start() # run in a different thread so gui doesnt freeze
